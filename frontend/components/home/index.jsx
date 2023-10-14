b7661e64d46e0a00d300bf31cfdbab065bf85ebb
Marcelo <marcelorcarnauba@gmail.com>
2023-10-14 20:13:23 -0300

    avancos-na-estilizacao

--- frontend/components/home/index.jsx
+++ frontend/components/home/index.jsx
@@ -2,9 +2,10 @@
 
 import './login.css'
 
+import Link from 'next/link'
+
 import Image from 'next/image'
 import GoogleImg from '@/public/google-logo.png'
-import Link from 'next/link'
 
 export default function Login() {
     return (
@@ -22,8 +23,13 @@ export default function Login() {
                     <p>Ir para o Gmail</p>
                 </div>
 
-                <div>
-                    4
+                <div className='w-full relative'>
+                    <input id='inputEmail' className='w-full rounded h-14 border border-gray-400 border-opacity-30' type='email'/>
+                    <label className='absolute left-4 text-color-cinza' htmlFor='inputEmail'>E-mail ou telefone</label>
+                </div>
+
+                <div className='w-full flex justify-start'>
+                    <p className='text-color-btn font-medium text-sm'>Esqueceu seu e-mail?</p>
                 </div>
 
                 <div>
@@ -32,7 +38,7 @@ export default function Login() {
                     </p>
                 </div>
 
-                <div>6</div>
+                <div>7</div>
             </div>
 
             <div>
